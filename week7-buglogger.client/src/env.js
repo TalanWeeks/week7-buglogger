c706af4eb575c145020f3a358bc49d6c074e273f
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-10-01 13:51:18 -0600

    init

---
+++ week7-buglogger.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'demodayz1.us.auth0.com'
+export const clientId = 'Gg4ZtWERZAo7oL2CMAYzX0PICtVHla17'
+export const audience = 'https://StudentAuth.to'
